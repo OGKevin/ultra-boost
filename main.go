b6660be96430832e9c5939a3ca601b3e2ffbe9fe
Kevin Hellemun <17928966+OGKevin@users.noreply.github.com>
2021-08-05 11:38:38 +0200

    details (#5)

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/heptiolabs/healthcheck"
 	"net/http"
 
 	"github.com/prometheus/client_golang/prometheus/promhttp"
@@ -11,7 +12,13 @@ func main() {
 	fmt.Print(buildPrintStr())
 	fmt.Print("\n")
 
+	health := healthcheck.NewHandler()
+
+	// Normally /metrics would be on a diff port
 	http.Handle("/metrics", promhttp.Handler())
+	http.HandleFunc("/-/ready", health.ReadyEndpoint)
+	http.HandleFunc("/-/healthy", health.LiveEndpoint)
+
 
 	fmt.Print("Starting http server on :8080\n")
 	_ = http.ListenAndServe(":8080", nil)
