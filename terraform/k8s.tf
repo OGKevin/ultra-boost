6cc4a89172a494a8887d12c0820ede19101b1adf
Kevin Hellemun <17928966+OGKevin@users.noreply.github.com>
2021-08-05 11:07:52 +0200

    No need for autoscaling

--- terraform/k8s.tf
+++ terraform/k8s.tf
@@ -1,13 +1,11 @@
-resource "digitalocean_kubernetes_cluster" "dev-cluster" {
-  name    = "dev-cluster"
+resource "digitalocean_kubernetes_cluster" "cluster" {
+  name    = "cluster"
   region  = "ams3"
   version = "1.21.2-do.2"
 
   node_pool {
     name       = "autoscale-worker-pool"
     size       = "s-8vcpu-16gb"
-    auto_scale = true
-    min_nodes  = 1
-    max_nodes  = 5
+    node_count = 1
   }
 }
